36d2af76dff1a87e1df93617425c5f27b827c6b5
VolxDarkTV <vinvenzo.foti@outlook.it>
2022-11-21 13:34:55 +0100

    Upload files and add array team

---
+++ js/index.js
@@ -0,0 +1,52 @@
+// creo l'Array con le info del team
+const ourTeam = [
+    // Barnett
+    {
+        'nome' : 'Wayne',
+        'cognome' : 'Barnett',
+        'role' : 'Founder & CEO',
+        'image' : 'immagine da sostituire'
+
+    },
+    // Caroll
+    {
+        'nome' : 'Angela',
+        'cognome' : 'Caroll',
+        'role' : 'Chief Editor',
+        'image' : 'immagine da sostituire'
+
+    },
+    // Gordon
+    {
+        'nome' : 'Walter',
+        'cognome' : 'Gordon',
+        'role' : 'Office Manager',
+        'image' : 'immagine da sostituire'
+
+    },
+    // Lopez
+    {
+        'nome' : 'Angela',
+        'cognome' : 'Lopez',
+        'role' : 'Social Media Manager',
+        'image' : 'immagine da sostituire'
+
+    },
+    // Estrada
+    {
+        'nome' : 'Scott',
+        'cognome' : 'Estrada',
+        'role' : 'Developer',
+        'image' : 'immagine da sostituire'
+
+    },
+    // Ramos
+    {
+        'nome' : 'Barbara',
+        'cognome' : 'Ramos',
+        'role' : 'Graphic Designer',
+        'image' : 'immagine da sostituire'
+
+    },
+]
+
